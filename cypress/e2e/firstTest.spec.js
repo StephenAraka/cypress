272dab6f681590922849eef892f468c4b89d6113
StephenAraka11 <stephen.araka@jumia.com>
2022-12-30 12:09:37 +0300

    Test structure

---
+++ cypress/e2e/firstTest.spec.js
@@ -0,0 +1,33 @@
+
+
+describe('Our first suite', () => {
+
+  it('first test', () => {
+
+  });
+
+  it('second test', () => {
+
+  });
+
+  it('third test', () => {
+
+  });
+
+});
+
+describe('Our second suite', () => {
+
+  it('first test', () => {
+
+  });
+
+  it('second test', () => {
+
+  });
+
+  it('third test', () => {
+
+  });
+
+});
