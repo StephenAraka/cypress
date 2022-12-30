19ccf6284dcc255c1b36eb0ef4a45db04c96a1e2
StephenAraka11 <stevenaraka@gmail.com>
2022-12-30 12:17:16 +0300

    Test structure

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,6 +1,23 @@
+/*
+  1. describe, then tests inside describe
+  2. can also have describe inside a descibe
+  3. beforeEach = hook called before every test
+*/
+
 
 
 describe('Our first suite', () => {
+
+  describe('Our suite section', () => {
+
+    beforeEach('code called before each test', () => {
+      // Put repetitive code here - eg login functionality
+    });
+
+    it('Some test name', () => {
+
+    });
+  });
 
   it('first test', () => {
 
